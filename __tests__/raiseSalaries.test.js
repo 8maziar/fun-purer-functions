44af0dc04090f4dda57d8be7c17ce2612c0c872e
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-24 14:45:36 +0100

    add new test to raise salary

--- __tests__/raiseSalaries.test.js
+++ __tests__/raiseSalaries.test.js
@@ -1,12 +1,12 @@
 const raiseSalaries = require("../raiseSalaries");
 
 describe("should return a new array of employees with their salary now increased by the given percentage increase", () => {
+  const actual = [
+    { name: "Alice", salary: 3000 },
+    { name: "Bob", salary: 2000 },
+    { name: "Vel", salary: 4500 },
+  ];
   test("return modified arrayed that is not mutated", () => {
-    const actual = [
-      { name: "Alice", salary: 3000 },
-      { name: "Bob", salary: 2000 },
-      { name: "Vel", salary: 4500 },
-    ];
     const expected = [
       { name: "Alice", salary: 3300 },
       { name: "Bob", salary: 2200 },
@@ -14,5 +14,14 @@ describe("should return a new array of employees with their salary now increased
     ];
 
     expect(raiseSalaries(actual, 10)).toEqual(expected);
+  });
+  test("return modified arrayed that is not mutated", () => {
+    const expected = [
+      { name: "Alice", salary: 3300 },
+      { name: "Bob", salary: 2200 },
+      { name: "Vel", salary: 4950 },
+    ];
+    raiseSalaries(actual, 10);
+    expect(actual[0].salary).toBe(3000);
   });
 });
